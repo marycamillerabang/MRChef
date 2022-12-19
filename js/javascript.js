d0d074ab7ab208d71dae5398b1fcdfa33ac12eee
Mary Camille Rabang <rabangmarycamille@gmail.com>
2022-12-20 01:37:04 +0800

    1
    1

---
+++ js/javascript.js
@@ -0,0 +1,37 @@
+/*First function is on the "index.html", it is a JavaScript for welcoming dialog */
+
+/*Second function is on the footer, it is a JavaScript for the auto date/year correction.*/
+
+
+function book(){alert("Booking Sucessfully, Click Ok To Fisnish Table Booking")
+		}//the function was printing the alert to notify that the table booking is successfull
+
+
+function ordersent(){alert("Order Sent Sucessfully, Click Ok to fisnish")
+		}//the function was printing the alert to notify that the order sent successfull
+
+function concernsent(){alert("Concern Sent Sucessfully, Click Ok to fisnish")
+		}//the function was printing the alert to notify that the order sent successfull
+
+
+function message(){alert("Your concern have been noted")
+		}//the function was printing the alert to notify that your message is noted.
+
+function menumrchef(){alert("YOU'RE OPENING MENU PAGE")
+		}//the function was printing the alert that you are in Menu page
+
+function reservationmrchef(){alert("YOU'RE OPENING RESERVATION PAGE")
+		}//the function was printing the alert that you are in Reservation Page
+
+function ordermrchef(){alert(" YOU'RE OPENING ORDER PAGE")
+		}//the function was printing the alert that you are in Order Page
+
+function aboutmrchef(){alert("YOU'RE OPENING ABOUT US PAGE")
+		}//the function was printing the alert that you are in About Us Page
+
+function contactmrchef(){alert("YOU'RE OPENING CONTACT PAGE")
+		}//the function was printing the alert that you are in Contact Page
+
+
+
+
